5f876c68f0d4a73f888187e6442fbe020a7e46e7
Colin Kiama <colinkiama@hotmail.co.uk>
2018-01-17 02:15:50 +0000

    done

---
+++ Averager/AverageCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Averager
+{
+    public sealed class AverageCalculator
+    
+    {
+        public static double GetAverage(IEnumerable<double> nums)
+        {
+            double[] numsToAverage = nums.ToArray();
+            int n = nums.Count();
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                double sum = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    sum += numsToAverage[i];
+                }
+
+                double average = sum / n;
+                return average;
+
+            }
+        }
+    }
+}
+
---
+++ Test/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Averager;
+using System.Diagnostics;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Test
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Works with anything that implements IEnumberable e.g Lists, arrays etc.
+
+            //List<double> numbers = new List<double> { 10, 10, 10, 10, 10 };
+            double[] numbers = { 10, 10, 10, 10, 10 };
+
+            
+            double average = AverageCalculator.GetAverage(numbers);
+            Debug.WriteLine(average);
+        }
+    }
+}
